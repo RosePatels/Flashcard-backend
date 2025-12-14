26af4621e74812b13cac841e37d25db9ecae8722
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2025-12-13 22:07:16 -0500

    Setup flashcard backend

---
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/FlashcardBackendApplication.java
@@ -0,0 +1,18 @@
+package com.flashcard_shreedev.flashcard_backend;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FlashcardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardBackendApplication.class, args);
+	}
+
+}
