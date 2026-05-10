760b9ee946c75ea9d6538f59b5d81791f1497def
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-05-10 18:31:10 -0400

    Ignore automatic datasource setup since db is not created

--- src/main/java/com/flashcard_shreedev/flashcard_backend/FlashcardBackendApplication.java
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/FlashcardBackendApplication.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //Disable automatic data source configuration since we do not have a database yet.
 public class FlashcardBackendApplication {
 	//Entry point of the application
 	public static void main(String[] args) {
