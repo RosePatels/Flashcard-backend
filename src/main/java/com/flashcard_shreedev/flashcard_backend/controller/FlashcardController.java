c33e615676003ca8f588f0a0157b40adbc8ad4e3
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-06-21 22:09:01 -0400

    Add create flashcard endpoint

--- src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
@@ -1,6 +1,7 @@
 package com.flashcard_shreedev.flashcard_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import com.flashcard_shreedev.flashcard_backend.classes.ApiResponse;
 import com.flashcard_shreedev.flashcard_backend.dto.FlashcardDto;
 import com.flashcard_shreedev.flashcard_backend.entity.Flashcard;
 import com.flashcard_shreedev.flashcard_backend.repository.FlashcardRepository;
@@ -22,7 +24,7 @@ public class FlashcardController {
     @Autowired
     private FlashcardRepository flashcardRepository;
     
-    @GetMapping
+    @GetMapping("/list")
     public List<FlashcardDto> getAllFlashcards() {
         return flashcardRepository.findAll().stream().map(fc -> {
             FlashcardDto flashcardDto = new FlashcardDto();
@@ -33,4 +35,19 @@ public class FlashcardController {
             return flashcardDto;
         }).toList();
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createFlashcard(@RequestBody FlashcardDto flashcardDto) {
+        if(flashcardDto.getQuestion() == null || flashcardDto.getAnswer() == null || flashcardDto.getCategory() == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Question, Answer and Category are required"));
+        }
+
+        Flashcard flashcard = new Flashcard();
+        flashcard.setQuestion(flashcardDto.getQuestion());
+        flashcard.setAnswer(flashcardDto.getAnswer());
+        flashcard.setCategory(flashcardDto.getCategory());
+        return ResponseEntity.ok(flashcardRepository.save(flashcard));
+    }
+
+
 }
