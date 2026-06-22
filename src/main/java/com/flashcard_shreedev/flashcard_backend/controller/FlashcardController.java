500dfdfdeb4dbcdf5f6f3fcbe1ecefffb92a1997
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-06-21 21:49:12 -0400

    Create flashcard dto

--- src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
@@ -3,10 +3,14 @@ package com.flashcard_shreedev.flashcard_backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import com.flashcard_shreedev.flashcard_backend.dto.FlashcardDto;
 import com.flashcard_shreedev.flashcard_backend.entity.Flashcard;
 import com.flashcard_shreedev.flashcard_backend.repository.FlashcardRepository;
 
@@ -18,8 +22,15 @@ public class FlashcardController {
     @Autowired
     private FlashcardRepository flashcardRepository;
     
-    @GetMapping("/list")
-    public  List<Flashcard> getAllFlashcards() {
-        return flashcardRepository.findAll();
+    @GetMapping
+    public List<FlashcardDto> getAllFlashcards() {
+        return flashcardRepository.findAll().stream().map(fc -> {
+            FlashcardDto flashcardDto = new FlashcardDto();
+            flashcardDto.setId(fc.getId());
+            flashcardDto.setQuestion(fc.getQuestion());
+            flashcardDto.setAnswer(fc.getAnswer());
+            flashcardDto.setCategory(fc.getCategory());
+            return flashcardDto;
+        }).toList();
     }
 }
