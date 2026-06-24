0a0733ea28282c400f7d051d8e8d2040b51981eb
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-06-23 21:46:58 -0400

    Add update and delete endpoints

--- src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/controller/FlashcardController.java
@@ -1,10 +1,14 @@
 package com.flashcard_shreedev.flashcard_backend.controller;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +53,31 @@ public class FlashcardController {
         return ResponseEntity.ok(flashcardRepository.save(flashcard));
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateFlashcard(@PathVariable Integer id, @RequestBody FlashcardDto flashcardDto) {
+        if(id == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Flashcard ID is required in order to update a flashcard"));
+        }
+        
+        Flashcard flashcard = flashcardRepository.findById(id).orElse(null);
+        if(flashcard == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Flashcard not found"));
+        }
 
+        flashcard.setQuestion(flashcardDto.getQuestion());
+        flashcard.setAnswer(flashcardDto.getAnswer());
+        flashcard.setCategory(flashcardDto.getCategory());
+        return ResponseEntity.ok(flashcardRepository.save(flashcard));
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteFlashcard(@PathVariable Integer id) {
+        Flashcard flashcard = flashcardRepository.findById(id).orElse(null);
+        if(flashcard == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Flashcard not found"));
+        }
+        flashcardRepository.delete(flashcard);
+        return ResponseEntity.ok(new ApiResponse(true, "Flashcard deleted successfully"));
+    }
 }
--- src/main/java/com/flashcard_shreedev/flashcard_backend/repository/FlashcardRepository.java
+++ src/main/java/com/flashcard_shreedev/flashcard_backend/repository/FlashcardRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flashcard_shreedev.flashcard_backend.entity.Flashcard;
 
-public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
+public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
     //Custom query methods can be defined here if needed
     
 }
